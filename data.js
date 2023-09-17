528da8e11cde24c0de921d93b7f88ebeff8b3241
M19604 <141754314+M19604@users.noreply.github.com>
2023-09-17 12:24:28 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const clientNames = {
-    LL5CW:"Daniel Benjamin",
     JWG90:"United States Government",
+    LL5CW:"Daniel Benjamin",
     O1512:"Luke Skutt"
 }
 const clientData = {
-    LL5CW:0,
     JWG90:26988913963832,
+    LL5CW:0,
     O1512:1
 }
