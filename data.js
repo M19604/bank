4dd736524078add40ff3cbbeb36dfcfd97e869f8
M19604 <141754314+M19604@users.noreply.github.com>
2023-09-20 15:39:09 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,16 @@
-import { clientData, clientNames } from "https://siprnet.w3spaces.com/data.js";
+const clientNames = {
+    JWG90:"United States Government",
+    11309:"Adam Olszewski",
+    D4CST:"James Bradsher",
+    HORQD:"Devin Rogers",
+    LL5CW:"Daniel Benjamin",
+    O1512:"Luke Skutt"
+}
+const clientData = {
+    JWG90:26988913963732,
+    11309:0,
+    D4CST:2,
+    HORQD:100,
+    LL5CW:50,
+    O1512:101
+}
