d49d04e83b255c9900f421f1831c5f93d75b4749
M19604 <141754314+M19604@users.noreply.github.com>
2023-09-23 12:23:58 -0400

    Update send.jsz

--- send.js
+++ send.js
@@ -2,12 +2,6 @@ function sendTransfer() {
     const scriptURL = 'https://script.google.com/macros/s/AKfycbxQxAtSfveHFGC9om1iAXzCAvbCttJ6ok0iZtA5gsuQ5rBQeN6BQGnD40kIoEXb5rWp/exec';
     const form = document.forms["Bank Tracking"];
     fetch(scriptURL, { method: 'POST', body: new FormData(form) });
-    // LOG TRANSFER
-    const scriptURL1 = "https://script.google.com/macros/s/AKfycbyWwtOicigudMa_WXq0z7OpGu16knVtoDzYYCwcMn7pfdIwvrAeEKxlJeVHHd8LU9NU/exec";
-    fetch(scriptURL1, { method:"POST", body: new FormData(form) });
-    // LOG ADD
-    const scriptURL2 = "https://script.google.com/macros/s/AKfycbyyMH_jQ5Z8B098nRoW2c9xc7p6XETMdOf-uXhpV4hgGohti874GtwstZR0Rbx_YYxU/exec";
-    fetch(scriptURL2, { method:"POST", body: new FormData(form) });
 }
 
 function sendAccount() {
