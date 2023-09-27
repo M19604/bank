fdd6c1bf1382737e7b0505fbfd6c5946af1bd2e3
M19604 <141754314+M19604@users.noreply.github.com>
2023-09-27 16:49:27 -0400

    Update acc.js

--- acc.js
+++ acc.js
@@ -1,19 +1,21 @@
 window.onload = function() {
-    for (let i = 0; i < Object.keys(clientData).length; i++) {
-        var span = document.createElement("div");
-        span.className = "span";
-        var count = i / 2;
-        if (count.toString().includes(".")) {
-            span.style.backgroundColor = "#f1f1f1";
+    setInterval(function() {
+        for (let i = 0; i < Object.keys(clientData).length; i++) {
+            var span = document.createElement("div");
+            span.className = "span";
+            var count = i / 2;
+            if (count.toString().includes(".")) {
+                span.style.backgroundColor = "#f1f1f1";
+            }
+            var nameElement = document.createElement("span");
+            nameElement.textContent = Object.values(clientNames)[i];
+            var valueElement = document.createElement("span");
+            valueElement.textContent = "$" + Object.values(clientData)[i].toLocaleString();
+            nameElement.className = "nameElement";
+            valueElement.className = "valueElement";
+            document.body.appendChild(span);
+            span.appendChild(nameElement);
+            span.appendChild(valueElement);
         }
-        var nameElement = document.createElement("span");
-        nameElement.textContent = Object.values(clientNames)[i];
-        var valueElement = document.createElement("span");
-        valueElement.textContent = "$" + Object.values(clientData)[i].toLocaleString();
-        nameElement.className = "nameElement";
-        valueElement.className = "valueElement";
-        document.body.appendChild(span);
-        span.appendChild(nameElement);
-        span.appendChild(valueElement);
-    }
+    })
 }
